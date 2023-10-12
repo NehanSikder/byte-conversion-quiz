42235f4c8e5626d6605207b505bec00b505a89d9
NehanSikder <arham.sikder@gmail.com>
2023-10-12 19:07:16 -0400

    Added feature to check answer

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Question(){
     // Units
@@ -6,46 +6,67 @@ function Question(){
     
 
     // Question Variables
-    const [questionValue, setQuestionValue] = useState<string>("");
-    const [questionUnit, setQuestionUnit] = useState<string>("");
-    const [convertToUnit, setConvertToUnit] = useState<string>("");
+    const [questionValue, setQuestionValue] = useState<number>(0);
+    const [questionUnitIndex, setQuestionUnitIndex] = useState<number>(0);
+    const [convertToUnitIndex, setConvertToUnitIndex] = useState<number>(1);
+    const [answerClassName, setanswerClassName] = useState<string>("");
+
 
     // Answer Variables
-    const [value, setValue] = useState<string>("");
-    const [unit, setUnit] = useState<string>("");
+    const [answer, setAnswer] = useState<string>("");
 
     const generateQuestion = () => {
         // Set random question value
         let min: number = 1;
         let max: number = 9000000;
-        let value: string  = (Math.floor(Math.random() * (max - min + 1)) + min).toString();
+        let value: number  = (Math.floor(Math.random() * (max - min + 1)) + min);
         setQuestionValue(value)
         // Set randm question unit
         min = 0;
         max = units.length - 1;
         let randomQuestionUnitIndex: number = (Math.floor(Math.random() * (max - min + 1)) + min);
-        setQuestionUnit(units[randomQuestionUnitIndex]);
+        setQuestionUnitIndex(randomQuestionUnitIndex);
         // Set random conversion unit
         let randomConversionUnitIndex: number = (Math.floor(Math.random() * (max - min + 1)) + min);
         while (randomConversionUnitIndex === randomQuestionUnitIndex){
             randomConversionUnitIndex = (Math.floor(Math.random() * (max - min + 1)) + min);
         }
-        setConvertToUnit(units[randomConversionUnitIndex]);
+        setConvertToUnitIndex(randomConversionUnitIndex);
 
 
     }
 
-    const verifyAnswer = () => void {
-        // if answer is valid set input field to green
-        // else set input field to red
+    const verifyAnswer = () => {
+        // calculate correct answer
+        let x: number =  1000 ** Math.abs(convertToUnitIndex - questionUnitIndex)
+        let expectedAnswer: number = 0;
+        if (questionUnitIndex < convertToUnitIndex){
+            expectedAnswer = questionValue / x;
+        } else {
+            expectedAnswer = questionValue * x;
+        }
+        if (expectedAnswer === +answer){
+            // if answer is valid set input field to green
+            setanswerClassName("correct");
+        } else {
+            // else set input field to red
+            setanswerClassName("wrong");
+
+
+
+        }
 
     }
+    // Generate a new question after the component renders
+    useEffect(() => {
+        generateQuestion();
+    }, [])
 
     return (
         <div>
             <h2>Questions</h2>
-            <h3>Convert: {questionValue} {questionUnit} to {convertToUnit}</h3>
-            <input type="text" />
+            <h3>Convert: {questionValue} {units[questionUnitIndex]} to {units[convertToUnitIndex]}</h3>
+            <label>Value: </label><input className={answerClassName} type="text" placeholder='10000' onChange={(e) => {setAnswer(e.target.value)}}/> 
             <br />
             <button onClick={generateQuestion}>Next</button>
             <button onClick={verifyAnswer}>Submit</button>
